10e0f0fd1e20f86fe4173c56841cb9eef98047a1
Illia Sheshyn <187636533+illiasheshyn@users.noreply.github.com>
2024-12-22 18:33:13 +1030

    value types: object (prelude)

--- value.h
+++ value.h
@@ -3,17 +3,16 @@
 
 #include "common.h"
 
-typedef enum {
-  VAL_BOOL,
-  VAL_NIL,
-  VAL_NUMBER,
-} ValueType;
+typedef struct Obj Obj;
+
+typedef enum { VAL_BOOL, VAL_NIL, VAL_NUMBER, VAL_OBJ } ValueType;
 
 typedef struct {
   ValueType type;
   union {
     bool boolean;
     double number;
+    Obj *obj;
   } as;
 } Value;
 
@@ -21,15 +20,18 @@ typedef struct {
 #define IS_BOOL(value) ((value).type == VAL_BOOL)
 #define IS_NIL(value) ((value).type == VAL_NIL)
 #define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+#define IS_OBJ(value) ((value).type == VAL_OBJ)
 
 // note: inherently unsafe to unwrap a `Value` without checking it's type
 #define AS_BOOL(value) ((value).as.boolean)
 #define AS_NUMBER(value) ((value).as.number)
+#define AS_OBJ(value) ((value).as.obj)
 
 // note: lift C value into Lox's `Value` value
 #define BOOL_VAL(value) ((Value){VAL_BOOL, {.boolean = value}})
 #define NIL_VAL ((Value){VAL_NIL, {.number = 0}})
 #define NUMBER_VAL(value) ((Value){VAL_NUMBER, {.number = value}})
+#define OBJ_VAL(object) ((Value){VAL_OBJ, {.obj = (Obj *)object}})
 
 typedef struct {
   int capacity;
