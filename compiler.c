076fb00326ac35979e62c7243e142a75bfec2b4d
Illia Sheshyn <187636533+illiasheshyn@users.noreply.github.com>
2024-12-30 00:35:56 +1030

    local variable declaration

--- compiler.c
+++ compiler.c
@@ -2,6 +2,7 @@
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "chunk.h"
 #include "common.h"
@@ -156,7 +157,15 @@ static void endCompiler() {
 
 static void beginScope() { current->scopeDepth++; }
 
-static void endScope() { current->scopeDepth--; }
+static void endScope() {
+  current->scopeDepth--;
+
+  while (current->localCount > 0 &&
+         current->locals[current->localCount - 1].depth > current->scopeDepth) {
+    emitByte(OP_POP);
+    current->localCount--;
+  }
+}
 
 static void expression();
 static void statement();
@@ -166,6 +175,43 @@ static void parsePrecedence(Precedence precedence);
 
 static uint8_t identifierConstant(Token *name) {
   return makeConstant(OBJ_VAL(copyString(name->start, name->length)));
+}
+
+static bool identifiersEqual(Token *a, Token *b) {
+  if (a->length != b->length)
+    return false;
+  return memcmp(a->start, b->start, a->length) == 0;
+}
+
+static void addLocal(Token name) {
+  if (current->localCount == UINT8_COUNT) {
+    error("too many local variables.");
+    return;
+  }
+
+  Local *local = &current->locals[current->localCount++];
+  local->name = name;
+  local->depth = current->scopeDepth;
+}
+
+static void declareVariable() {
+  if (current->scopeDepth == 0)
+    return;
+
+  Token *name = &parser.previous;
+  for (int i = current->localCount - 1; i >= 0; i--) {
+    // check all locals in current scope.
+    Local *local = &current->locals[i];
+    if (local->depth != -1 && local->depth < current->scopeDepth) {
+      // reached enclosing scope.
+      break;
+    }
+
+    if (identifiersEqual(name, &local->name)) {
+      error("already a variable with this name in this scope.");
+    }
+  }
+  addLocal(*name);
 }
 
 static void binary(bool canAssign) {
@@ -340,6 +386,11 @@ static void parsePrecedence(Precedence precedence) {
 
 static uint8_t parseVariable(const char *errorMessage) {
   consume(TOKEN_IDENTIFIER, errorMessage);
+
+  declareVariable();
+  if (current->scopeDepth > 0)
+    return 0;
+
   return identifierConstant(&parser.previous);
 }
 
@@ -355,6 +406,9 @@ static void block() {
 }
 
 static void defineVariable(uint8_t global) {
+  if (current->scopeDepth > 0) {
+    return;
+  }
   emitBytes(OP_DEFINE_GLOBAL, global);
 }
 
