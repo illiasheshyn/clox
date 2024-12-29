adfbfde2d53d21b0e5ff6197e23f0d7dfa68a3ec
Illia Sheshyn <187636533+illiasheshyn@users.noreply.github.com>
2024-12-29 17:30:35 +1030

    block statements

--- compiler.c
+++ compiler.c
@@ -154,6 +154,10 @@ static void endCompiler() {
 #endif
 }
 
+static void beginScope() { current->scopeDepth++; }
+
+static void endScope() { current->scopeDepth--; }
+
 static void expression();
 static void statement();
 static void declaration();
@@ -343,6 +347,13 @@ static ParseRule *getRule(TokenType type) { return &rules[type]; }
 
 static void expression() { parsePrecedence(PREC_ASSIGNMENT); }
 
+static void block() {
+  while (!check(TOKEN_RIGHT_BRACE) && !check(TOKEN_EOF)) {
+    declaration();
+  }
+  consume(TOKEN_RIGHT_BRACE, "expect '}' after block.");
+}
+
 static void defineVariable(uint8_t global) {
   emitBytes(OP_DEFINE_GLOBAL, global);
 }
@@ -409,6 +420,10 @@ static void declaration() {
 static void statement() {
   if (match(TOKEN_PRINT)) {
     printStatement();
+  } else if (match(TOKEN_LEFT_BRACE)) {
+    beginScope();
+    block();
+    endScope();
   } else {
     expressionStatement();
   }
