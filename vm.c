b52b20e6b0b2a1b840f71ef088dca42117bc010e
Illia Sheshyn <187636533+illiasheshyn@users.noreply.github.com>
2025-01-01 16:15:54 +1030

    while statements

--- vm.c
+++ vm.c
@@ -221,6 +221,11 @@ static InterpretResult run() {
         vm.ip += offset;
       break;
     }
+    case OP_LOOP: {
+      uint16_t offset = READ_SHORT();
+      vm.ip -= offset;
+      break;
+    }
     case OP_RETURN: {
       // exit interpreter.
       return INTERPRET_OK;
