af0c07ffe4de582c2e6bc8c58619d427bf14ad19
Illia Sheshyn <187636533+illiasheshyn@users.noreply.github.com>
2025-01-04 00:33:04 +1030

    function objects

--- object.h
+++ object.h
@@ -1,6 +1,7 @@
 #ifndef clox_object_h
 #define clox_object_h
 
+#include "chunk.h"
 #include "common.h"
 #include "value.h"
 
@@ -8,12 +9,15 @@
 
 // separate inline function to prevent evaluating argument expression (and
 // associated side effects) for `value` more than once. (e.g., IS_STRING(POP()))
+#define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
 #define IS_STRING(value) isObjType(value, OBJ_STRING)
 
+#define AS_FUNCTION(value) ((ObjFunction *)AS_OBJ(value))
 #define AS_STRING(value) ((ObjString *)AS_OBJ(value))
 #define AS_CSTRING(value) (((ObjString *)AS_OBJ(value))->chars)
 
 typedef enum {
+  OBJ_FUNCTION,
   OBJ_STRING,
 } ObjType;
 
@@ -22,6 +26,13 @@ struct Obj {
   struct Obj *next;
 };
 
+typedef struct {
+  Obj obj;
+  int arity;
+  Chunk chunk;
+  ObjString *name;
+} ObjFunction;
+
 struct ObjString {
   Obj obj;
   int length;
@@ -29,6 +40,7 @@ struct ObjString {
   uint32_t hash;
 };
 
+ObjFunction *newFunction();
 ObjString *takeString(char *chars, int length);
 ObjString *copyString(const char *chars, int length);
 void printObject(Value value);
