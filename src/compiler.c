a1ce9d4801543e132f0c32f0a2d7025b674983b4
Illia Sheshyn <187636533+illiasheshyn@users.noreply.github.com>
2025-02-19 23:31:13 +1030

    class declarations

--- src/compiler.c
+++ src/compiler.c
@@ -620,6 +620,18 @@ static void function(FunctionType type) {
   }
 }
 
+static void classDeclaration() {
+  consume(TOKEN_IDENTIFIER, "expect class name.");
+  uint8_t nameConstant = identifierConstant(&parser.previous);
+
+  declareVariable();
+  emitBytes(OP_CLASS, nameConstant);
+  defineVariable(nameConstant);
+
+  consume(TOKEN_LEFT_BRACE, "expect '{' before class body.");
+  consume(TOKEN_RIGHT_BRACE, "expect '}' after class body.");
+}
+
 static void funDeclaration() {
   uint8_t global = parseVariable("expect function name.");
   markInitialized();
@@ -770,7 +782,9 @@ static void synchronize() {
 }
 
 static void declaration() {
-  if (match(TOKEN_FUN)) {
+  if (match(TOKEN_CLASS)) {
+    classDeclaration();
+  } else if (match(TOKEN_FUN)) {
     funDeclaration();
   } else if (match(TOKEN_VAR)) {
     varDeclaration();
