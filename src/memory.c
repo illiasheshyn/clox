268c588285a319fc2701dacf0038dbc8e4340b42
Illia Sheshyn <187636533+illiasheshyn@users.noreply.github.com>
2025-02-20 23:59:47 +1030

    instances of classes

--- src/memory.c
+++ src/memory.c
@@ -87,6 +87,12 @@ static void freeObject(Obj *object) {
     FREE(ObjFunction, object);
     break;
   }
+  case OBJ_INSTANCE: {
+    ObjInstance *instance = (ObjInstance *)object;
+    freeTable(&instance->fields);
+    FREE(ObjInstance, object);
+    break;
+  }
   case OBJ_NATIVE: {
     FREE(ObjNative, object);
     break;
@@ -144,6 +150,12 @@ static void blackenObject(Obj *object) {
   case OBJ_UPVALUE:
     markValue(((ObjUpvalue *)object)->closed);
     break;
+  case OBJ_INSTANCE: {
+    ObjInstance *instance = (ObjInstance *)object;
+    markObject((Obj *)instance->klass);
+    markTable(&instance->fields);
+    break;
+  }
   case OBJ_NATIVE:
   case OBJ_STRING:
     break;
--- src/vm.c
+++ src/vm.c
@@ -114,6 +114,11 @@ static bool callValue(Value callee, int argCount) {
     switch (OBJ_TYPE(callee)) {
     case OBJ_CLOSURE:
       return call(AS_CLOSURE(callee), argCount);
+    case OBJ_CLASS: {
+      ObjClass *klass = AS_CLASS(callee);
+      vm.stackTop[-argCount - 1] = OBJ_VAL(newInstance(klass));
+      return true;
+    }
     case OBJ_NATIVE: {
       NativeFn native = AS_NATIVE(callee);
       Value result = native(argCount, vm.stackTop - argCount);
