1a2963bd481a1d0b8591c6903e70806f4b77f44a
Illia Sheshyn <187636533+illiasheshyn@users.noreply.github.com>
2025-02-22 13:44:14 +1030

    method declarations

--- src/memory.c
+++ src/memory.c
@@ -72,6 +72,8 @@ static void freeObject(Obj *object) {
 
   switch (object->type) {
   case OBJ_CLASS: {
+    ObjClass *klass = (ObjClass *)object;
+    freeTable(&klass->methods);
     FREE(ObjClass, object);
     break;
   }
@@ -131,6 +133,7 @@ static void blackenObject(Obj *object) {
   case OBJ_CLASS: {
     ObjClass *klass = (ObjClass *)object;
     markObject((Obj *)klass->name);
+    markTable(&klass->methods);
     break;
   }
   case OBJ_CLOSURE: {
